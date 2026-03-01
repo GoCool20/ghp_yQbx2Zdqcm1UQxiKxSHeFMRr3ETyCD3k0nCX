b14754b2c903054dd9d80704d40b5c9fb1290a34
Gokul Shinde <shindegokul7841@gmail.com>
2026-03-01 13:20:47 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7153129151-358ca",
+  "appId": "1:61642195232:web:daebc72b35ffc054f91745",
+  "apiKey": "AIzaSyBYjUeMajD82f-_f6xIFOO_-Fn1sFsPyHM",
+  "authDomain": "studio-7153129151-358ca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "61642195232"
+};
